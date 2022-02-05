b6e96b9037dbb0114beecb5201535ecb096736ac
mustaq-amd <97461845+mustaq-amd@users.noreply.github.com>
2022-02-05 15:41:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var imageArr=["https://assets.msn.com/bundles/v1/weather/latest/SunnyDay.svg",
     "https://assets.msn.com/bundles/v1/weather/latest/SunnyDay.svg"]
 async function dailydata(lon,lat){   
     try {
-        let res=await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&units=metric&appid=${key}`);
+        let res=await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&units=metric&appid=${key}`);
         console.log("res : ",res)
     let data= await res.json();
          console.log("data : ",data)
@@ -26,7 +26,7 @@ async function dailydata(lon,lat){
 async function getWeatherData() {
     try {
         let city = document.getElementById("city").value;
-        let res = await fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`);
+        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`);
         let data = await res.json();
         console.log("data",data);
         showWeather(data);
